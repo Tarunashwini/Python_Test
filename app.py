58741ee5be809460d3f8c180463ae55290533d82
Tarun Ashwini <78437808+Tarunashwini@users.noreply.github.com>
2024-06-07 12:02:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return '<h1 style="color:#F333FF; text-align:center;">Hello, world! Tarun Ashwini ! Commit 1</h1>'
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
